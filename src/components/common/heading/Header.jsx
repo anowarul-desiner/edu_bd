56080f5bf0c8c335f2bbd4a29879259cfdd0d758
Anowarul Islam <borosele03@gmail.com>
2022-10-10 16:46:02 +0600

    navbar create done!

--- src/components/common/heading/Header.jsx
+++ src/components/common/heading/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Head from './Head'
 import './header.css'
 
@@ -6,6 +7,18 @@ const Header = () => {
     return (
         <>
             <Head />
+            <Header>
+                <nav className='flexSB'>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/Courses">All Courses</Link></li>
+                        <li><Link to="/About">About</Link></li>
+                        <li><Link to="/team">Team</Link></li>
+                        <li><Link to="/pricing">Pricing</Link></li>
+                        <li><Link to="/journal">Journal</Link></li>
+                    </ul>
+                </nav>
+            </Header>
         </>
     )
 }
