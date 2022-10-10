933ad0275522a601d733726cdf141ca58908012f
Anowarul Islam <borosele03@gmail.com>
2022-10-10 17:34:08 +0600

    nav flex

--- src/components/common/heading/Header.jsx
+++ src/components/common/heading/Header.jsx
@@ -1,24 +1,28 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import Head from './Head'
 import './header.css'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
     return (
         <>
             <Head />
-            <Header>
+            <header>
                 <nav className='flexSB'>
-                    <ul>
+                    <ul className='flexSB'>
                         <li><Link to="/">Home</Link></li>
-                        <li><Link to="/Courses">All Courses</Link></li>
-                        <li><Link to="/About">About</Link></li>
+                        <li><Link to="/courses">All Courses</Link></li>
+                        <li><Link to="/about">About</Link></li>
                         <li><Link to="/team">Team</Link></li>
                         <li><Link to="/pricing">Pricing</Link></li>
                         <li><Link to="/journal">Journal</Link></li>
+                        <li><Link to="/contact">Contact</Link></li>
                     </ul>
+                    <div className="start">
+                        <div className="button">GET CERTIFICATE</div>
+                    </div>
                 </nav>
-            </Header>
+            </header>
         </>
     )
 }
